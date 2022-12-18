3bed9ef8c3416401ed85508f87d94004e77f8035
FelipeAmorimDev <felipeamorim.go@gmail.com>
2022-12-18 11:20:27 -0300

    Adicionado function que remove itens da lista

--- app.js
+++ app.js
@@ -6,11 +6,18 @@ formAddTodo.addEventListener('submit', event => {
   const taskValue = formAddTodo.add.value.trim()
   const itemElement = document.createElement('li')
 
-  if(taskValue.length > 0){
+  if (taskValue.length > 0) {
     itemElement.innerHTML = `<span>${taskValue}</span><i class="far fa-trash-alt delete"></i>`
-    itemElement.setAttribute('class','list-group-item d-flex justify-content-between align-items-center')
+    itemElement.setAttribute('class', 'list-group-item d-flex justify-content-between align-items-center')
     taskList.insertAdjacentElement('beforeend', itemElement)
 
     event.target.reset()
+  }
+})
+
+taskList.addEventListener('click', ({ target }) => {
+const isADeletableIten = target.matches('.delete')
+  if (isADeletableIten) {
+    target.parentElement.remove()
   }
 })
