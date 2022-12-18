a924c74b1910b2240977d9f98f102cd430d002c8
FelipeAmorimDev <felipeamorim.go@gmail.com>
2022-12-18 08:04:15 -0300

    Adicionado function que adicionar os itens a lista

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
+const formAddTodo = document.querySelector('.form-add-todo')
+const taskList = document.querySelector('.todos-container')
 
+formAddTodo.addEventListener('submit', event => {
+  event.preventDefault()
+  const taskValue = formAddTodo.add.value.trim()
+  const itemElement = document.createElement('li')
+
+  if(taskValue.length > 0){
+    itemElement.innerHTML = `<span>${taskValue}</span><i class="far fa-trash-alt delete"></i>`
+    itemElement.setAttribute('class','list-group-item d-flex justify-content-between align-items-center')
+    taskList.insertAdjacentElement('beforeend', itemElement)
+
+    event.target.reset()
+  }
+})
